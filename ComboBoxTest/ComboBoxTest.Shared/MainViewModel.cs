9dfa1a6d600e09f15fdf1807a7156da15ce7a2d5
Adam Love <ALove@Kahua.com>
2020-12-02 14:58:53 -0500

    Add project files.

---
+++ ComboBoxTest/ComboBoxTest.Shared/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ComboBoxTest.Shared
+{
+    public class MainViewModel
+    {
+        public Dictionary<string, string> Items { get; }
+
+        public string CurrentKey { get; set; }
+
+        public MainViewModel()
+        {
+            Items = new Dictionary<string, string>();
+            Items.Add("Test1Key", "Test1");
+            Items.Add("Test2Key", "Test2");
+            Items.Add("Test3Key", "Test3");
+
+            CurrentKey = "Test2Key";
+        }
+    }
+}
---
+++ ComboBoxTest/ComboBoxTest.Skia.Tizen/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tizen.Applications;
+using Uno.UI.Runtime.Skia;
+
+namespace ComboBoxTest.Skia.Tizen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new TizenHost(() => new ComboBoxTest.App(), args);
+            host.Run();
+        }
+    }
+}
